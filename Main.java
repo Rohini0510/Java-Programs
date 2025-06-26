df540a899bda24edab6f06812e44fef0ded80abe
Rohini0510 <rohinidambhare508@gmail.com>
2025-06-26 20:27:08 +0530

    getting input of a string and type promotion

--- Main.java
+++ Main.java
@@ -2,3 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     } }
+/*
+ TO commit changes in git, intellij uses
+ ctrl+k: to commit
+to push : Ctrl+Shift+k
+And to pull vcs>Git>Pull
+*/
---
+++ TypePromotion.java
@@ -0,0 +1,16 @@
+public class TypePromotion {
+    public static void main(String[] args) {
+        byte b=42;
+        char c='a';
+        int i =432;
+        double d= 2456.345;
+        float f= 43.6f;
+        double results= (f*b)+(i/c)+(d*c);
+        System.out.println(results);
+
+    }
+
+
+
+
+}
