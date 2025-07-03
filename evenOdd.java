f692d34cd619f9e590437b0c0f61b65730e52c20
Rohini0510 <rohinidambhare508@gmail.com>
2025-07-03 19:11:21 +0530

    Even and odd number

---
+++ evenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.println(" Enter the number which is to be evaluated as even or odd:");
+        int ans=in.nextInt();
+        if (ans%2==0){
+            System.out.println(+ans+ " is even");
+        }else {
+            System.out.println(+ans+ " is odd");
+        }
+    }
+}
